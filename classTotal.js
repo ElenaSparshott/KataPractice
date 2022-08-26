24eab6e1aa8e84d61c70e2e47dec2a4c90989e46
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-08-26 14:56:45 +0200

    classTotal

---
+++ classTotal.js
@@ -0,0 +1,25 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+
+function betterThanAverage(classPoints, yourPoints) {
+  
+  classPointsTotal = classPoints.reduce((a, b) => a + b, 0)
+  
+  console.log(classPointsTotal)
+  
+  totalPoints = classPointsTotal + yourPoints
+  
+  numberInClass = classPoints.length+1
+//   console.log(totalPoints)
+  
+  if(yourPoints > totalPoints/numberInClass){
+    return true
+  }else{
+    return false
+  }
+};
