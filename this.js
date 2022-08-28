3523b5c4bc4800867a365cc0c92d837bee2f17d4
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-08-28 14:14:00 +0200

    'this'

---
+++ this.js
@@ -0,0 +1,23 @@
+// What is this?
+// In JavaScript, the this keyword refers to an object.
+
+// Which object depends on how this is being invoked (used or called).
+
+// The this keyword refers to different objects depending on how it is used:
+
+
+// CodeWars -  Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+
+String.prototype.isUpperCase = function() {
+    return this == this.toUpperCase()
+}
