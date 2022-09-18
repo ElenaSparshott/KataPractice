dd221cfb091cdd791fd455a10a168e6c65a98fbd
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-09-18 09:02:19 +0100

    filter

---
+++ if/filter.js
@@ -0,0 +1,10 @@
+// List Filtering
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+
+function filter_list(l) {  
+  let newArray = x => Number.isInteger(x);
+  let newList = l.filter(newArray)
+  return newList
+}
