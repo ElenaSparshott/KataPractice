10d86a10cf92518c50d32431299b453f882d15b8
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-09-19 08:54:14 +0100

    sort

--- if/filter.js
+++ if/filter.js
@@ -2,9 +2,12 @@
 
 // In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
+function superArr(x) {
+    return Number.isInteger(x)
+}
 
 function filter_list(l) {  
-  let newArray = x => Number.isInteger(x);
-  let newList = l.filter(newArray)
+//   let newArray = x => Number.isInteger(x);
+  let newList = l.filter(superArr)
   return newList
 }
