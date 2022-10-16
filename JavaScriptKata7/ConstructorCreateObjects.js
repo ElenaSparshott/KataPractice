a3279f57da67d12ca2dfac6743d304b1b7384377
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-10-16 17:37:14 +0100

    FCC

---
+++ JavaScriptKata7/ConstructorCreateObjects.js
@@ -0,0 +1,24 @@
+// Use a Constructor to Create Objects
+// Here's the Bird constructor from the previous challenge:
+
+// function Bird() {
+//   this.name = "Albert";
+//   this.color  = "blue";
+//   this.numLegs = 2;
+// }
+
+// let blueBird = new Bird();
+// NOTE: this inside the constructor always refers to the object being created.
+
+// Notice that the new operator is used when calling a constructor. This tells JavaScript to create a new instance of Bird called blueBird. Without the new operator, this inside the constructor would not point to the newly created object, giving unexpected results. Now blueBird has all the properties defined inside the Bird constructor:
+// Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
+
+
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+
+let hound = new Dog();
