308984028e57f190eb3715ce7a72596f7c45ebe5
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-01-13 13:28:10 +0000

    opposite number eg, -1 to 1

---
+++ PythonKata8/Oppositenumber.py
@@ -0,0 +1,10 @@
+# Opposite number
+
+# Very simple, given an integer or a floating-point number, find its opposite.
+
+
+def opposite(number):
+    if(number >0):
+        return -abs(number)
+    else:
+        return abs(number)
