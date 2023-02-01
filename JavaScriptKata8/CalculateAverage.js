acade62af9b26f6aa8cb1ee17cf91049cad4fdb7
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-02-01 20:53:32 +0000

    imporved calculate average

--- JavaScriptKata8/CalculateAverage.js
+++ JavaScriptKata8/CalculateAverage.js
@@ -1,13 +1,25 @@
 // Calculate average
 
-function find_average(array) {
+// function find_average(array) {
   // empty array return 0
   //average = add numbers in array and divide by how many there are (index)
-    let total = 0;
+//     let total = 0;
+//     for(let x of array) {
+//         total += x;  
+//     }
+//     let average = total/array.length
+//     return average
+//   }
+
+  function find_average(array) {
+
+  if(array.length == 0) {
+    return 0
+  }
+  let total = 0;
     for(let x of array) {
         total += x;  
     }
     let average = total/array.length
     return average
   }
-  
