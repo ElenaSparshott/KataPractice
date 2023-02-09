28916c34bea23284358cc0cdefa003f1c12390dd
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-02-09 16:58:18 +0000

    grasshopper

---
+++ JavaScriptKata8/hero.js
@@ -0,0 +1,10 @@
+// Grasshopper - Terminal Game #1
+
+function Hero (name) {
+      this.name = name || "Hero"
+      this.position = "00"
+      this.experience = 0
+      this.health = 100
+      this.damage = 5
+      this.experence = 0   
+}
