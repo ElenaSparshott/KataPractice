94c0dba01c39ee85cd39aa479900bd8eae7b6852
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-02-09 12:38:45 +0000

    variables in PHP

---
+++ JavaScriptKata8/practice.php
@@ -0,0 +1,21 @@
+<?php
+  $movie = "___";
+// Add your code here:
+
+$movie = "Matrix";
+
+
+
+  echo "I'm a fickle person, my favorite movie used to be $movie.";
+
+  $old_favorite = $movie;
+  
+// Add a statement here:
+  
+  $movie = "All movies";
+
+  echo "\nBut now my favorite is $movie.";
+  
+// Add a statement below:
+
+echo " I like to watch {$movie}s.";
