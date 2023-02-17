3037ffffe3b76043e57bd37999e63ef25e3c6705
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-02-17 21:03:31 +0000

    cash register

--- JavaScriptKata8/casjhRegister.js
+++ JavaScriptKata8/casjhRegister.js
@@ -1,32 +1,38 @@
-const currencyUnit = {
-  "ONE HUNDRED":100,
-  "TWENTY":20,
-  "TEN":10,
-  "FIVE":5,
-  "DOLLAR":1,
-  "QUARTER":0.25,
-  "DIME":0.1,
-  "NICKEL":0.05,
-  "PENNY":0.01, 
+let currencyUnit = {
+  "ONE HUNDRED":10000,
+  "TWENTY":2000,
+  "TEN":100,
+  "FIVE":50,
+  "DOLLAR":10,
+  "QUARTER":25,
+  "DIME":10,
+  "NICKEL":5,
+  "PENNY":1, 
 }
+
+
 
 function checkCashRegister(price, cash, cid) {
-  let change = cash - price;
-
-  let coinsRequired = ""
-
-  for (let i in currencyValue) {
-    if(currencyValue[i][1] <= change){
-      coinsRequired += currencyValue[i][0]
-    //   console.log(coinsRequired)
-    console.log(currencyValue[1][coinsRequired])
-      return coinsRequired
-    }
-  }
-
+  let change = cash*100 - price*100;
   
 
+   let coinsRequired = ""
+   let totalCID = 0
+
+  for(let cash of cid){
+    totalCID += cash[1]*100
+  }
+
+  if(totalCID > change) {
+    return {status: "INSUFFICIENT_FUNDS", change: []}
+  } else if (totalCID == change)  {
+    return {status: "CLOSED", change: totalCID}
+  } else {
+
+  }
+
+
   return change;
-}
+} 
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
