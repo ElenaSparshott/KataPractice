8845694a9025f464e75d3e1f07b57a2ce8dab39f
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-02-22 17:52:19 +0000

    cash

--- JavaScriptKata8/casjhRegister.js
+++ JavaScriptKata8/casjhRegister.js
@@ -10,29 +10,33 @@ let currencyUnit = {
   "PENNY":1, 
 }
 
-
-
 function checkCashRegister(price, cash, cid) {
   let change = cash*100 - price*100;
-  
 
-   let coinsRequired = ""
-   let totalCID = 0
+  let coinsRequired = {}
+  let totalCID = 0
 
-  for(let cash of cid){
+  for(let cash of cid) {
     totalCID += cash[1]*100
   }
 
-  if(totalCID > change) {
+  console.log(totalCID);
+  console.log(change)
+
+  if(change > totalCID) {
     return {status: "INSUFFICIENT_FUNDS", change: []}
-  } else if (totalCID == change)  {
+  } else if (change === totalCID)  {
     return {status: "CLOSED", change: totalCID}
   } else {
-
+    let reversedCid = cid.reverse()
+    for(let coin of reversedCid) {
+        
+    }
   }
 
 
   return change;
 } 
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+let totalChange = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(totalChange)
