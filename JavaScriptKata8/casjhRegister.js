d1ac407d41332129588141069170cbd634b4a50d
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-02-23 13:14:44 +0000

    cashregistersolution

--- JavaScriptKata8/casjhRegister.js
+++ JavaScriptKata8/casjhRegister.js
@@ -1,9 +1,9 @@
 let currencyUnit = {
   "ONE HUNDRED":10000,
   "TWENTY":2000,
-  "TEN":100,
-  "FIVE":50,
-  "DOLLAR":10,
+  "TEN":1000,
+  "FIVE":500,
+  "ONE":100,
   "QUARTER":25,
   "DIME":10,
   "NICKEL":5,
@@ -13,30 +13,39 @@ let currencyUnit = {
 function checkCashRegister(price, cash, cid) {
   let change = cash*100 - price*100;
 
-  let coinsRequired = {}
   let totalCID = 0
 
   for(let cash of cid) {
     totalCID += cash[1]*100
   }
 
-  console.log(totalCID);
-  console.log(change)
-
   if(change > totalCID) {
     return {status: "INSUFFICIENT_FUNDS", change: []}
   } else if (change === totalCID)  {
-    return {status: "CLOSED", change: totalCID}
+    return {status: "CLOSED", change: cid}
   } else {
+    let theChange = []
     let reversedCid = cid.reverse()
+    
     for(let coin of reversedCid) {
-        
+       let coinsRequired = [coin[0], 0]
+        coin[1]=coin[1]*100
+        while(change >= currencyUnit[coin[0]] && coin[1] > 0) {
+          console.log(currencyUnit[coin[0]])
+          change -= currencyUnit[coin[0]]
+          coin[1] -= currencyUnit[coin[0]]
+          coinsRequired[1] += currencyUnit[coin[0]]/100
+        }
+        if(coinsRequired[1]>0) {
+          theChange.push(coinsRequired)
+          console.log(theChange)
+        }
     }
+    if(change > 0) {
+      return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+    return {status: "OPEN", change: theChange}
   }
-
-
-  return change;
 } 
 
-let totalChange = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-console.log(totalChange)
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
