8fc63a2916c9d3e1cb7d979e9a348696f4085d22
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-04-18 18:58:00 +0100

    while </=

--- JavaScriptKata7/GrowthOfAPopulation.js
+++ JavaScriptKata7/GrowthOfAPopulation.js
@@ -1,13 +1,28 @@
+// function nbYear(p0, percent, aug, p) {
+  
+//     let acc = p0;
+//     let year = 0;
+  
+//     while(acc < p){
+//       let newNumber = (acc * percent/100) + aug;
+//       console.log(newNumber)
+//       acc += newNumber
+//       year += 1;
+//     }
+//   return year
+// }
+
+
 function nbYear(p0, percent, aug, p) {
   
-    let acc = p0;
-    let year = 0;
+  let acc = p0;
+  let year = 0;
+
+  while(acc < p) {
+    let newNumber = Math.floor(acc * percent / 100) + aug;
+    acc += newNumber
+    year += 1;
+  }
   
-    while(acc < p){
-      let newNumber = (acc * percent/100) + aug;
-      console.log(newNumber)
-      acc += newNumber
-      year += 1;
-    }
   return year
 }
