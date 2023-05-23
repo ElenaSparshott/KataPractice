d218a9f97de84b1057185ef157759d6e06b25eba
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-05-23 13:24:44 +0100

    twice as old

---
+++ JavaScriptKata8/Twiceasold.js
@@ -0,0 +1,9 @@
+// Twice as old
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  
+  let twiceSonsAge =(sonYearsOld * 2) 
+  let yearsAgo = dadYearsOld - twiceSonsAge
+  return Math.abs(yearsAgo)
+}
+
