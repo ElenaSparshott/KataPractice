cb99ab38cb2cf983f309d9a0a9475581f41bd5d1
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-05-29 07:45:46 +0100

    includes

--- JavaScriptKata8/odd.js
+++ JavaScriptKata8/odd.js
@@ -319,3 +319,12 @@ function isDigit(s) {
     return false
   }
 }
+
+
+function validate(username, password){
+  var database = new Database();
+  if(password.includes("||")) {
+    return "Wrong username or password!"
+  } 
+  return database.login(username, password);
+}
