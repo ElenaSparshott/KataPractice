52001801f514ce5a0bd21cf1d33b8e49c0038b45
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-10-19 22:13:25 +0100

    Object.entries

--- JavaScriptKata8/Square.js
+++ JavaScriptKata8/Square.js
@@ -7,7 +7,34 @@ let squareArea = (A) => (A*2/Math.PI)**2
 // Are arrow functions odd?
 
 function odds(values){
-
   return values.filter( e => e%2 == 1);
+}
+
+
+
+// L1: Bartender, drinks!
+
+const database = {
+  "Jabroni": "Patron Tequila",
+  "School Counselor":	"Anything with Alcohol",
+  "Programmer":	"Hipster Craft Beer",
+  "Bike Gang Member":	"Moonshine",
+  "Politician":	"Your tax dollars",
+  "Rapper":	"Cristal"
+}
+
+const lowercaseDatabase = {}
+for (const [key, value] of Object.entries(database)) {
+  lowercaseDatabase[key.toLowerCase()] = value
+}
+
+function getDrinkByProfession(param){
   
+  let foundValue = lowercaseDatabase[param.toLowerCase()]
+  
+  if(foundValue) {
+    return foundValue
+  } else {
+    return "Beer"
+  } 
 }
