5a7a682400b0009317d6fb3dc139e69951469209
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-10-20 20:35:05 +0100

    Split and slice

--- JavaScriptKata8/Square.js
+++ JavaScriptKata8/Square.js
@@ -1,60 +1,76 @@
-// Square
+// // Square
 
-let squareArea = (A) => (A*2/Math.PI)**2
-
-
-
-// Are arrow functions odd?
-
-function odds(values){
-  return values.filter( e => e%2 == 1);
-}
+// let squareArea = (A) => (A*2/Math.PI)**2
 
 
 
-// L1: Bartender, drinks!
+// // Are arrow functions odd?
 
-const database = {
-  "Jabroni": "Patron Tequila",
-  "School Counselor":	"Anything with Alcohol",
-  "Programmer":	"Hipster Craft Beer",
-  "Bike Gang Member":	"Moonshine",
-  "Politician":	"Your tax dollars",
-  "Rapper":	"Cristal"
-}
+// function odds(values){
+//   return values.filter( e => e%2 == 1);
+// }
 
-const lowercaseDatabase = {}
-for (const [key, value] of Object.entries(database)) {
-  lowercaseDatabase[key.toLowerCase()] = value
-}
 
-function getDrinkByProfession(param){
+
+// // L1: Bartender, drinks!
+
+// const database = {
+//   "Jabroni": "Patron Tequila",
+//   "School Counselor":	"Anything with Alcohol",
+//   "Programmer":	"Hipster Craft Beer",
+//   "Bike Gang Member":	"Moonshine",
+//   "Politician":	"Your tax dollars",
+//   "Rapper":	"Cristal"
+// }
+
+// const lowercaseDatabase = {}
+// for (const [key, value] of Object.entries(database)) {
+//   lowercaseDatabase[key.toLowerCase()] = value
+// }
+
+// function getDrinkByProfession(param){
   
-  let foundValue = lowercaseDatabase[param.toLowerCase()]
+//   let foundValue = lowercaseDatabase[param.toLowerCase()]
   
-  if(foundValue) {
-    return foundValue
-  } else {
-    return "Beer"
-  } 
+//   if(foundValue) {
+//     return foundValue
+//   } else {
+//     return "Beer"
+//   } 
+// }
+
+// // Grasshopper - Object syntax debug
+
+// var rooms = {
+  
+//   first: {
+//     description: 'This is the first room',
+//     items: {
+//       chair: 'The old chair looks comfortable',
+//       lamp: 'This lamp looks ancient'
+//     },
+//   },
+//   second: {
+//     description: 'This is the second room',
+//     items: {
+//       couch: 'This couch looks like it would hurt your back',
+//       table: 'On the table there is an unopened bottle of water'
+//     },
+//   },
+// }
+
+// Remove First and Last Character Part Two
+
+function array(string) {
+  let a = string.split(",")
+  console.log(a)
+  if(a.length < 3) {
+    return null
+  }
+  let newArray =  a.slice(1, a.length - 1);
+  console.log(newArray)
+  let answer = newArray.join(" ")
+  return answer
 }
 
-// Grasshopper - Object syntax debug
 
-var rooms = {
-  
-  first: {
-    description: 'This is the first room',
-    items: {
-      chair: 'The old chair looks comfortable',
-      lamp: 'This lamp looks ancient'
-    },
-  },
-  second: {
-    description: 'This is the second room',
-    items: {
-      couch: 'This couch looks like it would hurt your back',
-      table: 'On the table there is an unopened bottle of water'
-    },
-  },
-}
