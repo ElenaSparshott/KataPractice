d87516fee863ee58d892c928be2d216fb47973f8
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-10-26 09:44:52 +0100

    leapyears

---
+++ JavaScriptKata7/ LeapYears.js
@@ -0,0 +1,12 @@
+// Leap Years
+
+function isLeapYear(year) {
+  if(year % 4 === 0 && year % 100 != 0) {
+    return true
+  } else if (year % 400 === 0 || year % 100 !== 0 && year % 4 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
