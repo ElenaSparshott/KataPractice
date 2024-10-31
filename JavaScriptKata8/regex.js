f01837dd3f773322c94de8ac68e08122489829f1
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-10-31 09:09:29 +0000

    toFixed()

--- JavaScriptKata8/regex.js
+++ JavaScriptKata8/regex.js
@@ -3,3 +3,17 @@
 function validateUsr(username) {
   return /^([a-z0-9_]){4,16}$/.test(username)
 }
+
+
+
+// Calculate Price Excluding VAT
+
+//return price without vat
+function excludingVatPrice(price){
+  if(price === null) {
+    return -1
+  }
+  return +(price/1.15).toFixed(2) 
+}
+
+
