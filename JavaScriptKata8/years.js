82504c83f84abd84d37adb3f3ba38a8321e4178e
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-12-03 09:29:44 +0000

    push and pop

--- JavaScriptKata8/years.js
+++ JavaScriptKata8/years.js
@@ -99,3 +99,33 @@ function buildString(...template){
 function all( arr, fun ){
   return arr.every(fun)
 }
+
+// Training JS #4: Basic data types--Array
+
+function getLength(arr){
+  //return length of arr
+  return arr.length
+}
+
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+
+function getLast(arr){
+  return arr[arr.length - 1]
+}
+
+function pushElement(arr){
+  var el=1;
+  arr.push(el)
+  return arr
+}
+
+function popElement(arr){
+  //pop an element from arr
+  arr.pop()
+  return arr
+}
+
+
